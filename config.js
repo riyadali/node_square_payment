d454f20f98055c38b94371a57f74f7680f945188
riyadali <riyadali@gmail.com>
2019-02-21 16:18:40 -0500

    Update and rename config.json to config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+// from this link https://stackoverflow.com/questions/36962601/node-js-how-to-use-environment-variables-in-json-file
+module.exports = {
+  config: {
+            "sandbox": {    
+                "squareApplicationId": process.env.APPLICATION_ID_SANDBOX,
+                "squareAccessToken": process.env.ACCESS_TOKEN_SANDBOX,
+                "squareLocationId": process.env.LOCATION_ID_SANDBOX
+            },  
+            "production": {     
+                "squareApplicationId": process.env.APPLICATION_ID_PROD,
+                "squareAccessToken": process.env.ACCESS_TOKEN_PROD,
+                "squareLocationId": process.env.LOCATION_ID_PROD
+            }
+          }
+};
