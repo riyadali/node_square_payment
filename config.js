3a518acee6220630eb928841be8553174078b6ac
riyadali <riyadali@gmail.com>
2019-02-22 12:31:54 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // from this link https://stackoverflow.com/questions/36962601/node-js-how-to-use-environment-variables-in-json-file
 module.exports = {
-  config: {
+  "config": {
             "sandbox": {    
                 "squareApplicationId": process.env.APPLICATION_ID_SANDBOX,
                 "squareAccessToken": process.env.ACCESS_TOKEN_SANDBOX,
