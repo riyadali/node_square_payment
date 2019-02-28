68dd9f77cc81879213a895a38fd2902048982391
riyadali <riyadali@gmail.com>
2019-02-28 15:21:01 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -62,84 +62,12 @@ router.post('/process-payment', function(req,res,next){
  router.post('/process-checkout', function(req,res,next){
  
   var idempotency_key = require('crypto').randomBytes(64).toString('hex');
-  var request_body = {
-      idempotency_key: idempotency_key,
-      order: {
-        reference_id: 'reference_id',
-        line_items: [
-          {
-            name: 'Printed T Shirt',
-            quantity: '2',
-            base_price_money: {amount: 1500, currency: 'USD'},
-            discounts: [
-              {
-                name: '7% off previous season item',
-                percentage: '7'
-              },
-              {
-                name: '$3 off Customer Discount',
-                amount_money: {amount: 300, currency: 'USD'}
-              }
-            ]
-          },
-          {
-            name: 'Slim Jeans',
-            quantity: '1',
-            base_price_money: {amount: 2500, currency: 'USD'}
-          },
-          {
-            name: 'Woven Sweater',
-            quantity: '3',
-            base_price_money: {amount: 3500, currency: 'USD'},
-            discounts: [
-              {
-                name: '$11 off Customer Discount',
-                amount_money: {amount: 1100, currency: 'USD'}
-              },
-              {
-                name: 'Fair Trade Tax',
-                percentage: '5'
-              }
-            ]
-          },
-        ],
-        discounts: [
-          {
-            name: "Father's day 12% OFF",
-            percentage: '12'
-          },
-          {
-            name: 'Global Sales $55 OFF',
-            amount_money: {amount: 5500, currency: 'USD'}
-          }
-        ],
-        taxes: [
-          {
-            name: 'Sales Tax',
-            type: 'ADDITIVE',
-            percentage: '8.5'
-          }
-        ]
-      },
-      ask_for_shipping_address: true,
-      merchant_support_email: 'merchant+support@website.com',
-      pre_populate_buyer_email: 'example@email.com',
-      pre_populate_shipping_address: {
-        address_line_1: '1455 Market St',
-        address_line_2: 'Suite 600',
-        locality: 'San Francisco',
-        administrative_district_level_1: 'CA',
-        postal_code: '94103',
-        country: 'US',
-        first_name: 'Jane',
-        last_name: 'Doe'
-      }
-      // redirect_url: 'https://www.example.com/checkout-order-confirm'
-    }; // end definition of request body 
-
-    var checkout_api = new squareConnect.CheckoutApi();
+  var request_body = req.body;
+  request_body.idempotency_key = idempotency_key;
+  
+  var checkout_api = new squareConnect.CheckoutApi();
 	
-    checkout_api.createCheckout(config.squareLocationId, request_body).then(function(data) {
+  checkout_api.createCheckout(config.squareLocationId, request_body).then(function(data) {
 		/*
 		var json= JSON.stringify(data);
 		res.render('process-payment', {
