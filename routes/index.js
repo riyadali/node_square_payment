de21fc0481783dca396bb16dffcaf679ea565e8f
riyadali <riyadali@gmail.com>
2019-03-05 13:33:34 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,7 @@ router.get('/', function(req, res, next) {
 router.get('/list-catalog', function(req, res, next) {
 	// Set the app and location ids for sqpaymentform.js to use
 	let queryTypes = req.query.types;
-	console.log("query string ..."+JSON.stringify(req.query)+);
+	console.log("query string ..."+JSON.stringify(req.query));
 	if (typeof queryTypes === 'undefined') { // types not specified as query param
 	   return res.sendStatus(400);
 	}
