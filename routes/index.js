740a612ecf50291393eae77c7b94d456ff4e8b63
riyadali <riyadali@gmail.com>
2019-03-05 11:13:50 -0500

    Square checkout api (#4)
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,9 @@ router.get('/', function(req, res, next) {
 	});
 });
 
+// Controller that handles interface with Square transaction api. 
+// On the client side a nonce is generated that is associated with a payment request (credit card)
+// This nonce is used here to complete the charge using Suqare's transaction API
 router.post('/process-payment', function(req,res,next){
 	var request_params = req.body;
 
@@ -49,6 +52,39 @@ router.post('/process-payment', function(req,res,next){
 	   }
 	);
 
+});
+
+// Controller that handles interface with Square's checkout API.
+// On the client side an order is generated and then passed to the controller  (Currently no order details is passed in.
+// A contrived order is created and sent to the checkout api .. but hopefully this will change in the future so that
+// the client's order is captured and then sent to Square's checkoput)
+
+ router.post('/process-checkout', function(req,res,next){
+ 
+  var idempotency_key = require('crypto').randomBytes(64).toString('hex');
+  var request_body = req.body;
+  request_body.idempotency_key = idempotency_key;
+  
+  var checkout_api = new squareConnect.CheckoutApi();
+	
+  checkout_api.createCheckout(config.squareLocationId, request_body).then(function(data) {
+		/*
+		var json= JSON.stringify(data);
+		res.render('process-payment', {
+			'title': 'Payment Successful',
+			'result': json
+		}); */
+		return res.json(data);
+	}, function(error) {
+		/*
+		res.render('process-payment', {
+			'title': 'Payment Failure',
+			'result': error.response.text
+		}
+		*/		
+		return next(error);
+	   }
+	);
 });
 
 module.exports = router;
