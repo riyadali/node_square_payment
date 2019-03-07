005103a42b50172709d3176b343aa356a7b01c0d
riyadali <riyadali@gmail.com>
2019-03-07 12:57:42 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,17 @@ var express = require('express');
 var router = express.Router();
 var auth = require('./auth');
 
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	// Set the app and location ids for sqpaymentform.js to use
+	res.render('index', {
+		'title': 'Make Payment',
+		'square_application_id': config.squareApplicationId,
+		'square_location_id': config.squareLocationId
+	});
+});
+
 router.use('/transaction', require('./transaction'));
 router.use('/checkout', require('./checkout'));
 router.use('/catalog', require('./catalog'));
